0c6dfdecade92f7caa61c42a9fe2365d9800fed6
Woong <parkpkww@gmail.com>
2026-03-20 15:25:00 +0900

    [feat] chat page 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import ChatPage from "./pages/ChatPage.tsx";
 
 const  App = () => {
   return (
-    <div> hi chat app </div>
+    <>
+      <div> hi chat app</div>
+      <ChatPage/>
+    </>
   )
 }
 
